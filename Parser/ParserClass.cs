a288169cf7d6659239e9b74ee37c8da92edffc32
KorsunPavel <pavel.korsun1@gmail.com>
2017-09-09 12:25:53 +0300

    - first init

---
+++ Parser/ParserClass.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser{
+    public class ParserClass {
+        public Dictionary<string, int> ParseTest(string text) {
+            StringBuilder sb = new StringBuilder();
+            Dictionary<string, int> pairs = new Dictionary<string, int>(StringComparer.InvariantCultureIgnoreCase);
+            for (int i = 0; i < text.Length; i++)
+            {
+
+                if (Char.IsLetter(text[i]))
+                {
+                    sb.Append(text[i]);
+                    if (i == text.Length - 1)
+                    {
+                        AddWordToDictioanary(sb, pairs);
+                    }
+                }
+                else
+                {
+                    if (CheckForDash(i, text, sb)) continue;
+                    AddWordToDictioanary(sb, pairs);
+                }
+            }
+            return pairs;
+        }
+
+        private bool CheckForDash(int i, string text, StringBuilder sb) {
+            if (i > 0 && i < text.Length - 1 && IsDash(text[i]))
+            {
+                if (Char.IsLetter(text[i - 1]) && Char.IsLetter(text[i + 1]))
+                {
+                    sb.Append('-');
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool IsDash(char ch) {
+            if (ch == '-' || ch == '\u2011' || ch == '\u2010') {
+                return true;
+            }
+            return false;
+        }
+
+        private  void AddWordToDictioanary(StringBuilder sb, Dictionary<string, int> pairs) {
+
+            string shell = sb.ToString();
+            sb.Clear();
+            if (shell.Length > 2)
+            {
+                if (pairs.ContainsKey(shell))
+                {
+                    pairs[shell]++;
+                }
+                else
+                {
+                    pairs.Add(shell, 1);
+                }
+            }
+        }
+    }
+}
---
+++ ParserTests/ParserUnitTests.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Parser;
+using System.Collections.Generic;
+using FluentAssertions;
+using System.Text;
+
+namespace ParserTests {
+    [TestClass]
+    public class ParserUnitTests {
+        [TestMethod]
+        public void ShouldParseDashes() {
+            string text = $"te\u2011st te\u2010st te-st -test -cat-cat-cat- are--results -- - test-";
+            ParserClass parser = new ParserClass();
+            Dictionary<string, int> wordsAmount = parser.ParseTest(text);
+            wordsAmount.Should().NotBeNull();
+            wordsAmount.Should().NotBeEmpty();
+            wordsAmount.Should().HaveCount(5);
+            wordsAmount.Should().Contain("test", 2);
+            wordsAmount.Should().Contain("te-st", 3);
+            wordsAmount.Should().Contain("cat-cat-cat", 1);
+            wordsAmount.Should().Contain("are", 1);
+            wordsAmount.Should().Contain("results", 1);
+        }
+
+        [TestMethod]
+        public void ShouldParseSpaces() {
+            string text = " test test    test    ";
+            ParserClass parser = new ParserClass();
+            Dictionary<string, int> wordsAmount = parser.ParseTest(text);
+            wordsAmount.Should().NotBeNull();
+            wordsAmount.Should().NotBeEmpty();
+            wordsAmount.Should().HaveCount(1);
+            wordsAmount.Should().Contain("test", 3);
+        }
+
+
+        [TestMethod]
+        public void ShouldParsePunctuation() {
+            string text = "test, test: test! test? test.";
+            ParserClass parser = new ParserClass();
+            Dictionary<string, int> wordsAmount = parser.ParseTest(text);
+            wordsAmount.Should().NotBeNull();
+            wordsAmount.Should().NotBeEmpty();
+            wordsAmount.Should().HaveCount(1);
+            wordsAmount.Should().Contain("test", 5);
+        }
+        [TestMethod]
+        public void ShouldParseLessThanThreeChars() {
+            string text = "t te tes test tes te t";
+            ParserClass parser = new ParserClass();
+            Dictionary<string, int> wordsAmount = parser.ParseTest(text);
+            wordsAmount.Should().NotBeNull();
+            wordsAmount.Should().NotBeEmpty();
+            wordsAmount.Should().HaveCount(2);
+            wordsAmount.Should().Contain("tes", 2);
+            wordsAmount.Should().Contain("test", 1);
+        }
+
+        [TestMethod]
+        public void ShouldParseNewLines() {
+            string[] items = { "Cat", "Dog", "Cat" };
+            StringBuilder builder = new StringBuilder(
+                "Required:").AppendLine();
+            foreach (string item in items)
+            {
+                builder.Append(item).AppendLine();
+            }
+            string text = builder.ToString();
+
+            ParserClass parser = new ParserClass();
+            Dictionary<string, int> wordsAmount = parser.ParseTest(text);
+            wordsAmount.Should().NotBeNull();
+            wordsAmount.Should().NotBeEmpty();
+            wordsAmount.Should().HaveCount(3);
+            wordsAmount.Should().Contain("Required", 1);
+            wordsAmount.Should().Contain("Cat", 2);
+            wordsAmount.Should().Contain("Dog", 1);
+        }
+
+        [TestMethod]
+        public void ShouldIgnoreCase() {
+            string text = "cat cAt CAT";
+
+            ParserClass parser = new ParserClass();
+            Dictionary<string, int> wordsAmount = parser.ParseTest(text);
+            wordsAmount.Should().NotBeNull();
+            wordsAmount.Should().NotBeEmpty();
+            wordsAmount.Should().HaveCount(1);
+            wordsAmount.Should().Contain("cat", 3);
+            wordsAmount.Should().Contain("CAT", 3);
+        }
+
+    }
+}
